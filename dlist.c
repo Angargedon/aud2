666fb77764c6a8eaa0a3cf061af3f4ec4a75d9de
Angargedon <angar@outlook.de>
2025-12-12 16:08:44 +0100

    fickdichfickdeinvaterfickdeinemutter

--- dlist.c
+++ dlist.c
@@ -2,89 +2,114 @@
 #include <stdlib.h>
 #include "tools.h"
 
-typedef struct s_dnode{
-    void* data;
-    struct s_dnode* prev;
-    struct s_dnode* next;
-}dnode_t;
+typedef struct s_dnode
+{
+    void *data;
+    struct s_dnode *prev;
+    struct s_dnode *next;
+} dnode_t;
 
+dnode_t *dnode_create(void *data)
+{
+    dnode_t *create = malloc(sizeof(dnode_t));
 
-dnode_t* dnode_create(void* data){
-    dnode_t* create = malloc(sizeof(dnode_t));
-
-    if(create != NULL){
+    if (create != NULL)
+    {
         create->data = data;
+        create->next = NULL;
+        create->prev = NULL;
     }
-    else{
+    else
+    {
         printf("\nSpeichern nicht moeglich. Mit einem Upgrade auf iCloud+ erhalten Sie auf diverse Geraete mehr Speicher und zusaetzliche Funktionen, wie 'iCloud Privat-Relay', 'E-Mail Adresse verbergen' und 'HomeKit Secure Video'.\nSie koennen sogar ihr Abo mit Ihrer Familie teilen. Weitere Infos finden Sie auf apple.de/icloud\n");
     }
     return create;
 }
 
-
-void dnode_delete(dnode_t* node){
-    node->next->prev = node->prev;
-    node->prev->next = node->next;
+void dnode_delete(dnode_t *node)
+{
+    // node->next->prev = node->prev; <- dit macht doch remove_at oder insert_at. müssen wir hier also nicht machen. 
+    // node->prev->next = node->next;
     free(node);
 }
 
-
-dnode_t* dnode_get_next(dnode_t* node){
+dnode_t *dnode_get_next(dnode_t *node)
+{
     return node->next;
 }
 
-
-dnode_t* dnode_get_prev(dnode_t* node){
+dnode_t *dnode_get_prev(dnode_t *node)
+{
     return node->prev;
 }
 
-
-void* dnode_get_data(dnode_t* node){
+void *dnode_get_data(dnode_t *node)
+{
     return node->data;
 }
 
+typedef struct s_dlist
+{
+    dnode_t *first;
+    dnode_t *last;
+} dlist_t;
 
-typedef struct s_dlist{
-    dnode_t* first;
-    dnode_t* last;
-}dlist_t;
-
-
-dlist_t* dlist_create(){
-    dlist_t* create = malloc(sizeof(dlist_t));
-    if(create != NULL){
+dlist_t *dlist_create()
+{
+    dlist_t *create = malloc(sizeof(dlist_t));
+    if (create != NULL)
+    {
+        create->first = NULL;
+        create->last = NULL;
         return create;
     }
-    else {
+    else
+    {
         printf("\nSpeichern nicht moeglich. Mit einem Upgrade auf iCloud+ erhalten Sie auf diverse Geraete mehr Speicher und zusaetzliche Funktionen, wie 'iCloud Privat-Relay', 'E-Mail Adresse verbergen' und 'HomeKit Secure Video'.\nSie koennen sogar ihr Abo mit Ihrer Familie teilen. Weitere Infos finden Sie auf apple.de/icloud\n");
         return create;
     }
 }
 
+void dlist_remove_all(dlist_t *list)
+{
+    dnode_t *rmv = list->first;
+    while (rmv != NULL)
+    {
+        dnode_delete(rmv);
+        rmv = rmv->next;
+    }
+}
 
-void dlist_delete(dlist_t* list){
+void dlist_delete(dlist_t *list)
+{
+    dlist_remove_all(list);
     free(list);
 }
 
-
-dnode_t* dlist_get_head(dlist_t* list){
+dnode_t *dlist_get_head(dlist_t *list)
+{
     return list->first;
 }
 
-
-dnode_t* dlist_get_last(dlist_t* list){
+dnode_t *dlist_get_last(dlist_t *list)
+{
     return list->last;
 }
 
-
-dnode_t* get_node_at(dlist_t* list, int index){
-    if(list != NULL){
-        if(index <= 0) return list->first;
-        else{
-            dnode_t* tmp = list->first;
-            for(int i=0; i<index; i++){
+dnode_t *dlist_get_node_at(dlist_t *list, int index)
+{
+    if (list != NULL)
+    {
+        if (index <= 0)
+            return list->first;
+        else
+        {
+            dnode_t *tmp = list->first;
+            for (int i = 0; i < index; i++)
+            {
                 tmp = tmp->next;
-                if(i>index){
+                if (i > index)
+                {
                     printf("\nListe ist nicht so lang mach bessere Index.\n");
                     clearBuffer();
                     return NULL;
@@ -93,207 +118,240 @@ dnode_t* get_node_at(dlist_t* list, int index){
             return tmp;
         }
     }
-    else{
+    else
+    {
         printf("\nliste gibt nicht. mach mal neue\n");
         clearBuffer();
         return NULL;
     }
 }
 
-
-void* dlist_get_data_at(dlist_t* list, int index){
-    if(list != NULL){
-        dnode_t* get = get_node_at(list, index);
+void *dlist_get_data_at(dlist_t *list, int index)
+{
+    if (list != NULL)
+    {
+        dnode_t *get = dlist_get_node_at(list, index);
         return get->data;
     }
-    else{
+    else
+    {
         printf("\nJunge mach richtige Liste.\n");
         return NULL;
     }
 }
 
-
-int dlist_index_of(dlist_t *list, dnode_t *node){
-    if((list != NULL) && (node != NULL)){
+int dlist_index_of(dlist_t *list, dnode_t *node)
+{
+    if ((list != NULL) && (node != NULL))
+    {
         int i = 0;
-        for(dnode_t* tmp = list->first; tmp != node; tmp = tmp->next){
+        for (dnode_t *tmp = list->first; tmp != node; tmp = tmp->next)
+        {
             i++;
         }
         return i;
     }
-    else if(list == NULL){
+    else if (list == NULL)
+    {
         printf("\nListe ist kaputt ich brauch andere jetzt du kriegst -69.\n");
         clearBuffer();
         return -69;
     }
-    else{
+    else
+    {
         printf("\nKnoten gibt es nicht ich mach jetzt -69 bei dir.\n");
         clearBuffer();
         return -69;
     }
 }
 
-
-int dlist_length(dlist_t* list){
-    if(list != NULL){
-        int i = 0;
-        for(dnode_t* tmp = list->first; tmp != list->last; tmp = tmp->next){
-            i++;
-        }
-        return i;
-    }
-    else{
+int dlist_length(dlist_t *list)
+{
+    if (list == NULL)
+    {
         printf("\nListe geht nicht mach neue du Dummkopf du bist save kein taktisches Kamel.\nIch mach dir -69.\n");
         clearBuffer();
         return -69;
     }
+    int i = 0;
+    dnode_t *tmp = list->first;
+    while (tmp != NULL){
+        tmp = tmp->next;
+        i++;
+    }
+    return i;
 }
 
-
-void dlist_append(dlist_t* list, void* data){
-    if(list != NULL){
-        dnode_t* new = dnode_create(data);
-        
-        if(list->first != NULL){
-            new->prev = list->last;
-            list->last = list->last->next = new;
-        }
-        else{
-            list->first = new;
-            list->last = new;
-        }
-    }
-    else{
+void dlist_append(dlist_t *list, void *data)
+{
+    if (list == NULL)
+    {
         printf("\nJunge bist du behindert? Schreib mal richtige Liste jetzt.\n");
         clearBuffer();
         printf("\nReg mich nicht nochmal so auf.\n");
         clearBuffer();
+        return;
+    }
+    dnode_t *new = dnode_create(data);
+
+    if (list->first != NULL)
+    {
+        new->prev = list->last;
+        list->last = list->last->next = new;
+    }
+    else
+    {
+        list->first = new;
+        list->last = new;
     }
 }
 
+void dlist_prepend(dlist_t *list, void *data)
+{
+    if (list != NULL)
+    {
+        dnode_t *new = dnode_create(data);
 
-void dlist_prepend(dlist_t* list, void* data){
-    if(list != NULL){
-        dnode_t* new = dnode_create(data);
-        
-        if(list->first != NULL){
+        if (list->first != NULL)
+        {
             new->next = list->first;
             list->first = list->first->prev = new;
         }
-        else{
+        else
+        {
             list->first = new;
             list->last = new;
         }
     }
-    else{
+    else
+    {
         printf("\nJunge bist du behindert? Schreib mal richtige Liste jetzt.\n");
         clearBuffer();
     }
 }
 
-
-void dlist_insert_at(dlist_t* list, int index, void* data){
-    if(list != NULL){
-        dnode_t* new = dnode_create(data);
-
-        if(list->first != NULL){
-            dnode_t* tmp = get_node_at(list, index);
-            if(tmp == list->first) list->first = new;
-
-            new->next = tmp;
-            new->prev = tmp->prev;
-            tmp->prev->next = new;
-            tmp->prev = new;
-        }
-        else{
-            list->first = new;
-            list->last = new;
-        }
-    }
-    else{
+void dlist_insert_at(dlist_t *list, int index, void *data)
+{
+    if (list == NULL)
+    {
         printf("\nListe ist alle keine da.\n");
         clearBuffer();
+        return;
+    }
+    if (index <= 0)
+    {
+        dlist_prepend(list, data);
+        return;
+    }
+    if (index >= dlist_length(list))
+    {
+        dlist_append(list, data);
+        return;
+    }
+    dnode_t *new = dnode_create(data);
+    if (list->first != NULL)
+    {
+        dnode_t *tmp = dlist_get_node_at(list, index);
+        if (tmp == list->first)
+            list->first = data;
+
+        new->next = tmp;
+        new->prev = tmp->prev;
+        tmp->prev->next = new;
+        tmp->prev = new;
+    }
+    else
+    {
+        list->first = new;
+        list->last = new;
     }
 }
 
-
-void dlist_remove_at(dlist_t* list, int index){
-    if(index < 0){
+void dlist_remove_at(dlist_t *list, int index){
+    if (list == NULL)
+    {
+        printf("\nListe ist schon weg entweder du dachtest er ist noch da oder du kannst nicht schreiben lern mal.\n");
+        return;
+    }
+    if (index < 0)
+    {
         list->first = list->first->next;
         dnode_delete(list->first->prev);
+        return;
     }
-    
-    if(list != NULL){
-        dnode_t* rm = get_node_at(list, index);
-        if(rm != list->last) dnode_delete(rm);
-        else{
-            list->last = list->last->prev;
-            dnode_delete(rm);
-        }
+    if (index > dlist_length(list))
+    {
+        list->last = list->last->prev;
+        dnode_delete(list->last->next);
+        return;
     }
-    else{
-        printf("\nListe ist schon weg entweder du dachtest er ist noch da oder du kannst nicht schreiben lern mal.\n");
-    }
+    dnode_t *rm = dlist_get_node_at(list, index);
+    dnode_delete(rm);
 }
 
-
-
-void dlist_remove_all(dlist_t* list){
-    for(dnode_t* rmv = list->first; rmv != list->last->next; rmv = rmv->next){
-        dnode_delete(rmv);
-    }
-}
-
-
-dlist_t* dlist_copy(dlist_t *list){
-    if(list != NULL){
-        dlist_t* copy = dlist_create();
-        copy->first = dlist_get_head(list);
-        copy->last = dlist_get_last(list);
-        return copy;
-    }
-    else{
+dlist_t *dlist_copy(dlist_t *list){
+    if (list == NULL)
+    {
         printf("\nGibt keine Liste zum Kopieren mach mal kein faxen.\n");
         clearBuffer();
         return NULL;
     }
+    dlist_t *copy = dlist_create();
+    dnode_t *tmp = list->first;
+    while (tmp != NULL)
+    {
+        dlist_append(copy, tmp->data);
+        tmp = tmp->next;
+    }
+    return copy;
 }
 
-
-void dlist_reverse(dlist_t* list){
-    if(list != NULL){
-        dnode_t* tmp = list->first;
-        int end = dlist_length(list);
-        for(int i=0; i<end; i++){
-            tmp->next->prev = tmp->next->next;
-            tmp->next->next = tmp;
-            tmp = tmp->next;
-        }
-        list->last = list->first;
-        list->first = tmp;
-    }
-    else{
+void dlist_reverse(dlist_t *list)
+{
+    if (list == NULL)
+    {
         printf("\nKeine Liste hier wieso machst du so?\n");
         clearBuffer();
+        return;
+    }
+    dnode_t *current = list->first;
+    dnode_t *tmp = NULL;
+    while (current != NULL)
+    {
+        tmp = current->prev;
+        current->prev = current->next;
+        current->next = tmp;
+        current = current->prev;
+    }
+    if (list->first != NULL)
+    {
+        tmp = list->first;
+        list->first = list->last;
+        list->last = tmp;
     }
 }
 
-
-void dlist_concat(dlist_t* list1, dlist_t* list2){
-    if((list1 != NULL) && (list2 != NULL)){
+void dlist_concat(dlist_t *list1, dlist_t *list2)
+{
+    if ((list1 != NULL) && (list2 != NULL))
+    {
         list2->first->prev = list1->last;
         list1->last->next = list2->first;
 
         list1->last = list2->last;
         list2->first = list1->first;
 
-        dlist_remove_all(list2);
+        // dlist_remove_all(list2);
+        list2->first = NULL;
+        list2->last = NULL;
     }
-    else if(list2 == NULL){
+    else if (list2 == NULL)
+    {
         printf("\nErste Liste gibt es nicht schreib richtig.\n");
         clearBuffer();
     }
-    else{
+    else
+    {
         printf("\nZweite Liste gibt es nicht du Eierkopf\n");
     }
 }
